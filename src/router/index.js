1a689a4a809fdfae19fcee322991cf359730ccda
zb961026 <602153088@qq.com>
2019-04-01 17:57:18 +0800

    item页面路由改造

--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,23 @@ import '@/assets/css/reset.css'
 import '@/assets/css/header.css'
 import '@/assets/css/goodsList.css'
 import '@/assets/css/prompt.css'
+import '@/assets/css/item.css'
 Vue.use(Router);
 import Shop from '@/views/Shop'
+import Item from '@/views/item'
 
 export default new Router({
+    mode:'history',
     routes:[
         {
             path:'/',
             name:Shop,
             component:Shop
+        },
+        {
+            path:'/item',
+            name:'Item',
+            component:Item
         }
     ]
 })
