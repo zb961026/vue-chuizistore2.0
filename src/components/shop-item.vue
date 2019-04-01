687157f78c8a5f81a2ca98f64516ac48d96c64c2
zb961026 <602153088@qq.com>
2019-04-01 13:44:41 +0800

    商品列表完成

--- src/components/shop-item.vue
+++ src/components/shop-item.vue
@@ -1,20 +1,20 @@
 <template>
 						<div class="item">
 							<div>
-								<div class="item-img"><img :alt="item.name" :src="item.sku_info[0].ali_image+'?x-oss-process=image/resize,w_206/quality,Q_80/format,webp'" style="opacity: 1;">
+								<div class="item-img"><img :alt="item.name" :src="item.sku_info[itemIndex].ali_image+'?x-oss-process=image/resize,w_206/quality,Q_80/format,webp'" style="opacity: 1;">
 								</div>
 								<h6>{{item.name}}</h6>
 								<h3 >{{item.name_title}}</h3>
 								<div class="params-colors">
 									<ul class="colors-list">
-										<li v-for="sku,index in item.sku_info"><a href="javascript:;" @click="tableIndex(index)" :title="sku.spec_json.show_name"><img :src="'http://img01.smartisanos.cn/'+sku.spec_json.image+'20X20.jpg'"></a></li>
+										<li v-for="sku,index in item.sku_info"><a href="javascript:;" @click="tableIndex(index)" :class="{'active':index==itemIndex}" :title="sku.spec_json.show_name"><img :src="'http://img01.smartisanos.cn/'+sku.spec_json.image+'20X20.jpg'"></a></li>
 									</ul>
 								</div>
 								<div class="item-btns clearfix">
 									<span class="item-gray-btn"><a href="javascript:;" target="_blank">查看详情</a> </span><span class="item-blue-btn">加入购物车 </span>
 								</div>
 								<div class="item-price clearfix">
-									<i>¥</i><span>199.00</span>
+									<i>¥</i><span>{{item.price}}</span>
 								</div>
 								<div class="discount-icon">false</div>
 								<div class="item-cover">
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import '@/assets/css/reset.css'
 import '@/assets/css/header.css'
+import '@/assets/css/goodsList.css'
 Vue.use(Router);
 import Shop from '@/views/Shop'
 
