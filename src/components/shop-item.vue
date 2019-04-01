2c51fdacf6fc6cb474479e75ba2808f02c71d344
zb961026 <602153088@qq.com>
2019-04-01 12:16:20 +0800

    商品页面渲染传值

---
+++ src/components/shop-item.vue
@@ -0,0 +1,51 @@
+<template>
+						<div class="item">
+							<div>
+								<div class="item-img"><img :alt="item.name" :src="item.sku_info[0].ali_image+'?x-oss-process=image/resize,w_206/quality,Q_80/format,webp'" style="opacity: 1;">
+								</div>
+								<h6>{{item.name}}</h6>
+								<h3 >{{item.name_title}}</h3>
+								<div class="params-colors">
+									<ul class="colors-list">
+										<li v-for="sku,index in item.sku_info"><a href="javascript:;" @click="tableIndex(index)" :title="sku.spec_json.show_name"><img :src="'http://img01.smartisanos.cn/'+sku.spec_json.image+'20X20.jpg'"></a></li>
+									</ul>
+								</div>
+								<div class="item-btns clearfix">
+									<span class="item-gray-btn"><a href="javascript:;" target="_blank">查看详情</a> </span><span class="item-blue-btn">加入购物车 </span>
+								</div>
+								<div class="item-price clearfix">
+									<i>¥</i><span>199.00</span>
+								</div>
+								<div class="discount-icon">false</div>
+								<div class="item-cover">
+									<a href="javascript:;" target="_blank"></a>
+								</div>
+							</div>
+						</div>
+</template>
+
+<script>
+import '@/assets/css/goodsList.css'
+export default{
+	data(){
+		return{
+			itemIndex:0
+		}
+	},
+	props:{
+		item:{
+			type:Object
+		}
+	},
+	methods:{
+		tableIndex(index)
+		{
+			this.itemIndex=index;
+		}
+	}
+}
+</script>
+
+<style>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import '@/assets/css/reset.css'
 import '@/assets/css/header.css'
-import '@/assets/css/goodsList.css'
 Vue.use(Router);
 import Shop from '@/views/Shop'
 
